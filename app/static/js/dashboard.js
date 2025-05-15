2ec8bcfac46d7cda6be251271aedf37d9d97f584
ym-Tsai <24302286@student.uwa.edu.au>
2025-05-16 01:22:06 +0800

    adjust css and js and html for styling of 3 pages, and alert msgbox; also the README.md

--- app/static/js/dashboard.js
+++ app/static/js/dashboard.js
@@ -1,5 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     const today = new Date();
-    document.getElementById('current-date').textContent = today.toLocaleDateString('en-US', options);
     
+    // Define the format options for the date
+    const options = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    };
+
+    // Set the current date in the element with id 'current-date'
+    document.getElementById('current-date').textContent = today.toLocaleDateString('en-US', options);
 });
